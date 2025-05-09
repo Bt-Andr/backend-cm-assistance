45761af3a91408f7e66984642f81373aee3a7a30
Bt-Andr <Beteandr@yahoo.com>
2025-05-09 17:33:11 +0100

    const Tickets in dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/verifyToken");
 const User = require("../models/user");
+const Ticket = require("../models/ticket");
 
 router.get("/", verifyToken, async (req, res) => {
   try {
