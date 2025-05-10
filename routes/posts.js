a57744dbcd1f1b5aba48f1ca823c387b2d52f94c
Bt-Andr <Beteandr@yahoo.com>
2025-05-10 19:49:16 +0100

    feat2 correct route /api/tickets dans app.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../middleware/verifyToken");
-const Post = require("../models/post");
+const verifyToken = require("../middleware/verifyToken");
+const Post = require("../models/posts");
 
 // GET /api/posts?page=1&limit=5
 router.get("/", verifyToken, async (req, res) => {
